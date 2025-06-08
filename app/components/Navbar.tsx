b77b1ab9aff9c742cf5f2428cd3c7f08ec4cc903
Hadayetullah <hadayetullah30@gmail.com>
2025-06-08 18:39:25 +0600

    Limit width for the extra large devices.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -39,75 +39,75 @@ const Navbar = ({isDarkMode, setIsDarkMode}: NavbarProps) => {
     })
 
   return (
-    <>
-    <div className='fixed top-0 right-0 -z-10 w-11/12 translate-y-[-80%] dark:hidden'>
-        <Image src={assets.header_bg_color} alt='' className='w-full'/>
-    </div>
-    
-    <nav 
-        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] flex items-center justify-between py-4 z-50 
-            dark:bg-darktheme dark:shadow-white/20 ${isScrolled ? 'bg-[#ffffff50] shadow-sm backdrop-blur-lg' : ''}`}
-    >
-        <a href="#home" className='cursor-pointer mr-14 text-[28px] font-outfit '>
-            Hadayetullah
-        </a>
-
-        {/* <a href="">
-            <Image src={assets.logo} alt='' className='w-28 cursor-pointer mr-14'  />
-        </a> */}
-
-        <ul 
-            className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 
-             ${isScrolled ? '' : 'bg-[#ffffff50] shadow-sm dark:border dark:border-white/50 dark:bg-transparent'}`}
+    <div className='w-full'>
+        <div className='absolute top-0 right-0 -z-10 w-11/12 translate-y-[-80%] dark:hidden'>
+            <Image src={assets.header_bg_color} alt='' className='w-full'/>
+        </div>
+        
+        <nav 
+            className={`absolute top-0 left-0 right-0 mx-auto w-full px-5 lg:px-8 xl:px-[8%] flex items-center justify-between py-4 z-50 
+                dark:bg-darktheme dark:shadow-white/20 ${isScrolled ? 'bg-[#ffffff50] shadow-sm backdrop-blur-lg' : ''}`}
         >
-            <li><a className='font-ovo' href="#home">Home</a></li>
-            <li><a className='font-ovo' href="#about">About me</a></li>
-            <li><a className='font-ovo' href="#services">Services</a></li>
-            <li><a className='font-ovo' href="#work">My Work</a></li>
-            <li><a className='font-ovo' href="#contact">Contact me</a></li>
-        </ul>
-
-        <div className='flex items-center gap-4'>
-            <button 
-                onClick={()=> setIsDarkMode(!isDarkMode)} 
-                className='cursor-pointer'
-            >
-                <Image src={isDarkMode ? assets.sun_icon : assets.moon_icon} alt='' className='w-6' />
-            </button>
-
-            <a 
-                href="#contact" 
-                className='hidden md:flex items-center gap-3 px-10 border border-gray-500 rounded-full 
-                py-2.5 ml-4 font-ovo dark:border-white/50'
-            >
-                Contact 
-                <Image src={isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon} alt='' className='w-3' />
+            <a href="#home" className='cursor-pointer mr-14 text-[28px] font-outfit '>
+                Hadayetullah
             </a>
 
-            <button onClick={openMenu} className='block md:hidden ml-3 cursor-pointer'>
-                <Image src={isDarkMode ? assets.menu_white : assets.menu_black} alt='' className='w-6' />
-            </button>
-        </div>
+            {/* <a href="">
+                <Image src={assets.logo} alt='' className='w-28 cursor-pointer mr-14'  />
+            </a> */}
 
-        {/* -------------- Mobile Menu --------------- */}
-        <ul 
-            ref={sideMenuRef} 
-            className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 
-            h-screen bg-rose-50 transition duration-500 dark:bg-darkhover dark:text-white'
-        >
-            <button className='absolute right-6 top-6' onClick={closeMenu}>
-                <Image src={isDarkMode ? assets.close_white : assets.close_black} alt='' className='w-5 cursor-pointer' />
-            </button>
+            <ul 
+                className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 
+                ${isScrolled ? '' : 'bg-[#ffffff50] shadow-sm dark:border dark:border-white/50 dark:bg-transparent'}`}
+            >
+                <li><a className='font-ovo' href="#home">Home</a></li>
+                <li><a className='font-ovo' href="#about">About me</a></li>
+                <li><a className='font-ovo' href="#services">Services</a></li>
+                <li><a className='font-ovo' href="#work">My Work</a></li>
+                <li><a className='font-ovo' href="#contact">Contact me</a></li>
+            </ul>
 
-            <li><a onClick={closeMenu} className='font-ovo' href="#home">Home</a></li>
-            <li><a onClick={closeMenu} className='font-ovo' href="#about">About me</a></li>
-            <li><a onClick={closeMenu} className='font-ovo' href="#services">Services</a></li>
-            <li><a onClick={closeMenu} className='font-ovo' href="#work">My Work</a></li>
-            <li><a onClick={closeMenu} className='font-ovo' href="#contact">Contact me</a></li>
-        </ul>
+            <div className='flex items-center gap-4'>
+                <button 
+                    onClick={()=> setIsDarkMode(!isDarkMode)} 
+                    className='cursor-pointer'
+                >
+                    <Image src={isDarkMode ? assets.sun_icon : assets.moon_icon} alt='' className='w-6' />
+                </button>
 
-    </nav>
-    </>
+                <a 
+                    href="#contact" 
+                    className='hidden md:flex items-center gap-3 px-10 border border-gray-500 rounded-full 
+                    py-2.5 ml-4 font-ovo dark:border-white/50'
+                >
+                    Contact 
+                    <Image src={isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon} alt='' className='w-3' />
+                </a>
+
+                <button onClick={openMenu} className='block md:hidden ml-3 cursor-pointer'>
+                    <Image src={isDarkMode ? assets.menu_white : assets.menu_black} alt='' className='w-6' />
+                </button>
+            </div>
+
+            {/* -------------- Mobile Menu --------------- */}
+            <ul 
+                ref={sideMenuRef} 
+                className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 
+                h-screen bg-rose-50 transition duration-500 dark:bg-darkhover dark:text-white'
+            >
+                <button className='absolute right-6 top-6' onClick={closeMenu}>
+                    <Image src={isDarkMode ? assets.close_white : assets.close_black} alt='' className='w-5 cursor-pointer' />
+                </button>
+
+                <li><a onClick={closeMenu} className='font-ovo' href="#home">Home</a></li>
+                <li><a onClick={closeMenu} className='font-ovo' href="#about">About me</a></li>
+                <li><a onClick={closeMenu} className='font-ovo' href="#services">Services</a></li>
+                <li><a onClick={closeMenu} className='font-ovo' href="#work">My Work</a></li>
+                <li><a onClick={closeMenu} className='font-ovo' href="#contact">Contact me</a></li>
+            </ul>
+
+        </nav>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -33,14 +33,14 @@ export default function Home() {
   }, [isDarkMode])
 
   return (
-    <>
-    <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-    <Header isDarkMode={isDarkMode} />
-    <About isDarkMode={isDarkMode} />
-    <Services isDarkMode={isDarkMode} />
-    <Work isDarkMode={isDarkMode} />
-    <Contact isDarkMode={isDarkMode} />
-    <Footer isDarkMode={isDarkMode} />
-    </>
+    <div className="max-w-[1500px] relative mx-auto">
+      <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Header isDarkMode={isDarkMode} />
+      <About isDarkMode={isDarkMode} />
+      <Services isDarkMode={isDarkMode} />
+      <Work isDarkMode={isDarkMode} />
+      <Contact isDarkMode={isDarkMode} />
+      <Footer isDarkMode={isDarkMode} />
+    </div>
   );
 }
