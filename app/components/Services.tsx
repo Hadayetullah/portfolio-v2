01bb9b1acc15b13f6f6057cf3e10c0233f0745fb
Hadayetullah <hadayetullah30@gmail.com>
2025-06-29 07:09:30 +0600

    Make responsive Services section for the smallest devices.

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -40,7 +40,7 @@ const Services = (props: Props) => {
             initial={{ opacity: 0, y: -20 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.3 }}
-            className='text-center mb-2 text-lg font-ovo '
+            className='text-center mb-2 text-base xxs:text-lg font-ovo '
         >
             What I offer
         </motion.h4>
@@ -49,7 +49,7 @@ const Services = (props: Props) => {
             initial={{ opacity: 0, y: -20 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.5 }}
-            className='text-5xl text-center font-ovo'
+            className='h2 text-center font-ovo'
         >
                 My Services
         </motion.h2>
@@ -58,7 +58,7 @@ const Services = (props: Props) => {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.7 }}
-            className='text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo'
+            className='w-full max-w-[500px] text-sm xxs:text-base text-center mx-auto mt-5 mb-6 xxs:mb-12 font-ovo'
         >
             I offer a range of services to help you achieve your goals.
         </motion.p>
@@ -67,27 +67,27 @@ const Services = (props: Props) => {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 0.6, delay: 0.9 }}
-            className='grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-6 my-10'
+            className='grid grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))] xxs:grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-6 my-5 xxs:my-10'
         >
             {
                 serviceData.map(({icon, title, description, link}, index) => (
                     <motion.div 
-                        whileHover={{ scale: 1.05 }}
+                        whileHover={{ scale: 1 }}
                         key={index}
-                        className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-shadowblack 
-                        cursor-pointer hover:-translate-y-1 duration-500 dark:hover:shadow-white'
+                        className='border border-gray-400 rounded-lg px-2 xxs:px-8 py-6 xxs:py-12 hover:shadow-shadowblack 
+                        cursor-pointer hover:-translate-y-1 duration-300 dark:hover:shadow-white'
                     >
                         <div className={`w-12 p-2 rounded-lg bg-[#ff388b] text-white`}>
                             {icon}
                         </div>
 
-                        <h3 className='text-lg my-5 text-gray-700 dark:text-white'>{title}</h3>
+                        <h3 className='text-lg my-3 xxs:my-5 dark:text-white'>{title}</h3>
 
                         <p className='text-sm text-gray-600 leading-5 dark:text-white/80'>{description}</p>
 
                         <a 
                             href={link}
-                            className='flex items-center gap-2 text-sm mt-5'
+                            className='flex items-center gap-2 text-sm font-medium mt-5'
                         >
                             Read more <Image src={assets.right_arrow} alt='' className='w-4' />
                         </a>
